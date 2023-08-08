846faf98a834352bd7767c73ae4100919f1f944d
armandoarizam <armandoairzamartienz@gmail.com>
2023-08-08 17:47:03 -0500

    new project

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './App.css';
+import { TodoAdd } from './components/TodoAdd';
+import { TodoList } from './components/TodoList';
+import { useTodo } from './hooks/useTodo';
+
+function App() {
+	const {
+		todos,
+		todosCount,
+		pendingTodosCount,
+		handleNewTodo,
+		handleDeleteTodo,
+		handleCompleteTodo,
+		handleUpdateTodo,
+	} = useTodo();
+
+	return (
+		<>
+			<div className='card-to-do'>
+				<h1>Todo App</h1>
+				
+				<div className='add-todo'>
+					<br />
+					<br />
+					<TodoAdd handleNewTodo={handleNewTodo} />
+				</div>
+
+				<TodoList
+					todos={todos}
+					handleUpdateTodo={handleUpdateTodo}
+					handleDeleteTodo={handleDeleteTodo}
+					handleCompleteTodo={handleCompleteTodo}
+				/>
+			</div>
+		</>
+	);
+}
+
+export default App;
